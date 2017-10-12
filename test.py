92ddcd2e4b90c8f5a250834f2152b2b01ba5d681
Trevor Barron <barron.trevor@gmail.com>
2017-10-12 10:39:28 -0700

    more scripts

--- test.py
+++ test.py
@@ -14,12 +14,14 @@
 #         obs, rew, done, info = env.step(env.action_space.sample())
 
 
+import numpy as np
 import gym
 # import pybullet_envs
 import gym_x
 
+env = gym.make('MountainCarContinuousVisionX-v0')
 # env = gym.make('Walker2DBulletX-v0')
-env = gym.make('Walker2DVisionBulletX-v0')
+# env = gym.make('Walker2DVisionBulletX-v0')
 # env = gym.make('ChainX-v0')
 # env = gym.make('ChainVisionX-v0')
 
@@ -31,5 +33,12 @@ for i in range(10):
     while not done and i < 100:
         env.render()
         obs, rew, done, info = env.step(env.action_space.sample())
+        print (obs.min(), obs.max(), obs.dtype)
+        obs = obs.astype(np.uint8)
+        print (obs.dtype)
+        from PIL import Image
+        I = Image.fromarray(obs.reshape((84, 84)))
+        I.show()
+        input("")
         i += 1
 env.close()
