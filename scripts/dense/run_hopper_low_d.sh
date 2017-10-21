90d67a7d02dc119190b79a172c0e59a83f0cf5ed
Trevor Barron <barron.trevor@gmail.com>
2017-10-20 17:57:13 -0700

    tmux script updates

--- scripts/dense/run_hopper_low_d.sh
+++ scripts/dense/run_hopper_low_d.sh
@@ -1,5 +1,5 @@
 # LOG_DIR="/media/trevor/22c63957-b0cc-45b6-9d8f-173d9619fb73/outputs/imle/10_17_17/";
-LOG_DIR="/media/Backup/trevor1_data/data/outputs/imle/10_20_17/";
+LOG_DIR="/media/Backup/trevor1_data/data/outputs/imle/10_20_17/set2/";
 # LOG_DIR="test_dir/";
 DEFAULT_ARGS="--bnn-n-updates-per-step 500 --max-episode-steps 1000 --num-processes 1 --num-steps 2048 --entropy-coef 0 --ppo-epoch 10 --lr 3e-4 --gamma 0.99 --tau 0.95 --batch-size 64 --num-frames 1000000 --use-gae"
 
@@ -15,9 +15,23 @@ do
   mkdir -p $EXP_PATH
   tmux new-session -s imle_hopper_norm_eta_decay_$i "python main.py ${DEFAULT_ARGS} --imle --seed ${i} --log-dir ${EXP_PATH} --env-name HopperBulletX-v0 --eta-decay"
 
-  while tmux has-session
+  while true
   do
-    sleep 1;
+    running=0;
+    if tmux has-session -t imle_hopper_norm_$i;
+    then
+      running=1;
+      sleep 1;
+    fi
+    if tmux has-session -t imle_hopper_norm_eta_decay_$i;
+    then
+      running=1;
+      sleep 1;
+    fi
+    if [ $running -eq 0 ];
+    then
+      break;
+    fi
   done
 
   EXP_PATH="${LOG_DIR}/baseline/hopper_dense_rew_x_t1000/${i}/";
@@ -28,10 +42,23 @@ do
   mkdir -p $EXP_PATH
   tmux new-session -s vime_hopper_$i -d "python main.py ${DEFAULT_ARGS} --vime --seed ${i} --log-dir ${EXP_PATH} --env-name HopperBulletX-v0"
 
-  # All this to run two tasks at a time. Probably a better way but I'm not a bash expert
-  while tmux has-session
+  while true
   do
-    sleep 1;
+    running=0;
+    if tmux has-session -t ppo_hopper_$i;
+    then
+      running=1;
+      sleep 1;
+    fi
+    if tmux has-session -t vime_hopper_$i;
+    then
+      running=1;
+      sleep 1;
+    fi
+    if [ $running -eq 0 ];
+    then
+      break;
+    fi
   done
 
 done
