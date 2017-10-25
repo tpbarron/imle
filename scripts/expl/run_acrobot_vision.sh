4c42d160c956e061845c5df8e16ac6c74b1b956c
Trevor Barron <barron.trevor@gmail.com>
2017-10-25 09:17:12 -0700

    acro vision script, model change

--- scripts/expl/run_acrobot_vision.sh
+++ scripts/expl/run_acrobot_vision.sh
@@ -1,21 +1,20 @@
 # LOG_DIR="/media/trevor/22c63957-b0cc-45b6-9d8f-173d9619fb73/outputs/imle/10_17_17/";
 LOG_DIR="/home/users/tbarron/Documents/dev/ml/data/outputs/imle/10_18_17/";
-DEFAULT_ARGS="--bnn-n-updates-per-step 500 --max-episode-steps 500 --num-processes 1 --num-steps 2048 --entropy-coef 0 --ppo-epoch 10 --lr 3e-4 --gamma 0.99 --tau 0.95 --batch-size 64 --num-frames 500000 --num-stack 4 --use-gae"
+DEFAULT_ARGS="--bnn-n-updates-per-step 500 --max-episode-steps 1000 --num-processes 1 --num-steps 2048 --entropy-coef 0 --ppo-epoch 10 --lr 3e-4 --gamma 0.99 --tau 0.95 --batch-size 64 --num-frames 1000000 --use-gae --num-stack 3"
 
 # imle vision
-for i in 1 2 3 4 5
+for i in 1 2 3
 do
-  # EXP_PATH="${LOG_DIR}/baseline/acrobot_continuous_vision_x/${i}/";
-  # mkdir -p $EXP_PATH;
-  # python main.py $DEFAULT_ARGS --seed $i --log-dir $EXP_PATH --env-name "AcrobotContinuousVisionX-v0"
 
-  EXP_PATH="${LOG_DIR}/imle_baseline/acrobot_continuous_vision_x/${i}/";
-  mkdir -p $EXP_PATH;
-  python main.py $DEFAULT_ARGS --imle --seed $i --log-dir $EXP_PATH --env-name "AcrobotContinuousVisionX-v0"
+  ENV_NAME="${LOG_DIR}/baseline/acrobot_continuous_vision_x_t1000/${i}/";
+  mkdir -p $ENV_NAME;
+  python main.py $DEFAULT_ARGS --seed $i --log-dir $ENV_NAME --env-name "AcrobotContinuousVisionX-v0" &
 
-  EXP_PATH="${LOG_DIR}/imle_baseline/acrobot_continuous_vision_x_eta_decay/${i}/";
-  mkdir -p $EXP_PATH;
-  python main.py $DEFAULT_ARGS --imle --seed $i --log-dir $EXP_PATH --env-name "AcrobotContinuousVisionX-v0" --eta-decay
+  ENV_NAME="${LOG_DIR}/imle_baseline/acrobot_continuous_vision_x_t1000/${i}/";
+  mkdir -p $ENV_NAME;
+  python main.py $DEFAULT_ARGS --imle --seed $i --log-dir $ENV_NAME --env-name "AcrobotContinuousVisionX-v0" &
+
+  wait;
 done
 
 # # no explr
