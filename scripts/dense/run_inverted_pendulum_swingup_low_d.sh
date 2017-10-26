2d8ce773b19344b6f4e05f9a1f2a0dfa752e1625
Trevor Barron <barron.trevor@gmail.com>
2017-10-26 12:01:06 -0700

    pend scripts

--- scripts/dense/run_inverted_pendulum_swingup_low_d.sh
+++ scripts/dense/run_inverted_pendulum_swingup_low_d.sh
@@ -1,5 +1,5 @@
 # LOG_DIR="/media/trevor/22c63957-b0cc-45b6-9d8f-173d9619fb73/outputs/imle/10_17_17/";
-LOG_DIR="/media/Backup/trevor1_data/data/outputs/imle/10_20_17/set2/";
+# LOG_DIR="/media/Backup/trevor1_data/data/outputs/imle/10_20_17/set2/";
 # LOG_DIR="test_dir/";
 
 DEFAULT_ARGS="--bnn-n-updates-per-step 500 --max-episode-steps 1000 --num-processes 1 --num-steps 2048 --entropy-coef 0 --ppo-epoch 10 --lr 3e-4 --gamma 0.99 --tau 0.95 --batch-size 64 --num-frames 500000 --use-gae"
@@ -8,77 +8,17 @@ DEFAULT_ARGS="--bnn-n-updates-per-step 500 --max-episode-steps 1000 --num-proces
 # low dim
 for i in 1 2 3
 do
-  EXP_PATH="${LOG_DIR}/imle_baseline/hopper_dense_rew_x_t1000_enc_norm/${i}/";
-  mkdir -p $EXP_PATH
-  python main.py ${DEFAULT_ARGS} --imle --seed ${i} --log-dir ${EXP_PATH} --env-name HopperBulletX-v0
-
-  EXP_PATH="${LOG_DIR}/baseline/hopper_dense_rew_x_t1000/${i}/";
-  mkdir -p $EXP_PATH
-  python main.py ${DEFAULT_ARGS} --seed ${i} --log-dir ${EXP_PATH} --env-name HopperBulletX-v0 &
-
-  EXP_PATH="${LOG_DIR}/vime_baseline/hopper_dense_rew_x_t1000/${i}/";
-  mkdir -p $EXP_PATH
-  python main.py ${DEFAULT_ARGS} --vime --seed ${i} --log-dir ${EXP_PATH} --env-name HopperBulletX-v0 &
-
-  wait;
-done
-
-# no explr
-# low dim
-for i in 1 2 3
-do
   EXP_PATH="${LOG_DIR}/imle_baseline/inverted_pendulum_swingup_dense_rew_t1000_enc_norm/${i}/";
   mkdir -p $EXP_PATH
-  tmux new-session -s imle_inv_pend_norm_$i -d "python main.py ${DEFAULT_ARGS} --imle --seed ${i} --log-dir ${EXP_PATH} --env-name InvertedPendulumSwingupBulletEnv-v0"
-
-  EXP_PATH="${LOG_DIR}/imle_baseline/inverted_pendulum_swingup_dense_rew_x_t1000_enc_norm_eta_decay/${i}/";
-  mkdir -p $EXP_PATH
-  tmux new-session -s imle_inv_pend_norm_eta_decay_$i "python main.py ${DEFAULT_ARGS} --imle --seed ${i} --log-dir ${EXP_PATH} --env-name InvertedPendulumSwingupBulletEnv-v0 --eta-decay"
-
-  while true
-  do
-    running=0;
-    if tmux has-session -t imle_inv_pend_norm_$i;
-    then
-      running=1;
-      sleep 1;
-    fi
-    if tmux has-session -t imle_inv_pend_norm_eta_decay_$i;
-    then
-      running=1;
-      sleep 1;
-    fi
-    if [ $running -eq 0 ];
-    then
-      break;
-    fi
-  done
+  python main.py ${DEFAULT_ARGS} --imle --seed ${i} --log-dir ${EXP_PATH} --env-name InvertedPendulumSwingupBulletEnv-v0 &
 
   EXP_PATH="${LOG_DIR}/baseline/inverted_pendulum_swingup_dense_rew_x_t1000/${i}/";
   mkdir -p $EXP_PATH
-  tmux new-session -s ppo_inv_pend_$i -d "python main.py ${DEFAULT_ARGS} --seed ${i} --log-dir ${EXP_PATH} --env-name InvertedPendulumSwingupBulletEnv-v0"
+  python main.py ${DEFAULT_ARGS} --seed ${i} --log-dir ${EXP_PATH} --env-name InvertedPendulumSwingupBulletEnv-v0 &
 
   EXP_PATH="${LOG_DIR}/vime_baseline/inverted_pendulum_swingup_dense_rew_x_t1000/${i}/";
   mkdir -p $EXP_PATH
-  tmux new-session -s vime_inv_pend_$i -d "python main.py ${DEFAULT_ARGS} --vime --seed ${i} --log-dir ${EXP_PATH} --env-name HInvertedPendulumSwingupBulletEnv-v0"
+  python main.py ${DEFAULT_ARGS} --vime --seed ${i} --log-dir ${EXP_PATH} --env-name InvertedPendulumSwingupBulletEnv-v0 &
 
-  while true
-  do
-    running=0;
-    if tmux has-session -t ppo_inv_pend_$i;
-    then
-      running=1;
-      sleep 1;
-    fi
-    if tmux has-session -t vime_inv_pend_$i;
-    then
-      running=1;
-      sleep 1;
-    fi
-    if [ $running -eq 0 ];
-    then
-      break;
-    fi
-  done
-
+  wait;
 done
--- scripts/dense/run_inverted_pendulum_swingup_vision.sh
+++ scripts/dense/run_inverted_pendulum_swingup_vision.sh
@@ -1,21 +1,24 @@
+# LOG_DIR="/media/trevor/22c63957-b0cc-45b6-9d8f-173d9619fb73/outputs/imle/10_17_17/";
+# LOG_DIR="/media/Backup/trevor1_data/data/outputs/imle/10_20_17/set2/";
 # LOG_DIR="test_dir/";
-LOG_DIR="/media/trevor/22c63957-b0cc-45b6-9d8f-173d9619fb73/outputs/imle/10_19_17/";
-# LOG_DIR="/home/users/tbarron/Documents/dev/ml/data/outputs/imle/";
-DEFAULT_ARGS="--bnn-n-updates-per-step 500 --max-episode-steps 1000 --num-processes 1 --num-steps 2048 --entropy-coef 0 --ppo-epoch 10 --lr 3e-4 --gamma 0.99 --tau 0.95 --batch-size 64 --num-frames 1000000 --num-stack 4 --use-gae"
+
+DEFAULT_ARGS="--bnn-n-updates-per-step 500 --max-episode-steps 1000 --num-processes 1 --num-steps 2048 --entropy-coef 0 --ppo-epoch 10 --lr 3e-4 --gamma 0.99 --tau 0.95 --batch-size 64 --num-frames 1000000 --use-gae --num-stack 3"
 
 # no explr
-# high dim
-for i in 1 2 3 4 5
+# low dim
+for i in 1 2 3
 do
-  EXP_PATH="baseline/inverted_pendulum_swingup_vision_dense_rew_t1000/${i}/";
-  mkdir -p "${LOG_DIR}/${EXP_PATH}";
-  python main.py $DEFAULT_ARGS --seed $i --log-dir "${LOG_DIR}/${EXP_PATH}" --env-name "InvertedPendulumSwingupVisionBulletEnv-v0"
-done
+  EXP_PATH="${LOG_DIR}/imle_baseline/inverted_pendulum_swingup_vision_dense_rew_t1000_enc_norm/${i}/";
+  mkdir -p $EXP_PATH
+  python main.py ${DEFAULT_ARGS} --imle --seed ${i} --log-dir ${EXP_PATH} --env-name InvertedPendulumSwingupVisionBulletEnv-v0 &
 
-# imle vision
-# for i in 1 2 3 4 5
-# do
-#   EXP_PATH="imle_baseline/inverted_pendulum_swingup_vision_dense_rew/${i}/";
-#   mkdir -p "${LOG_DIR}/${EXP_PATH}";
-#   python main.py $DEFAULT_ARGS --imle --seed $i --log-dir "${LOG_DIR}/${EXP_PATH}" --env-name "InvertedPendulumSwingupVisionBulletEnv-v0"
-# done
+  EXP_PATH="${LOG_DIR}/baseline/inverted_pendulum_swingup_vision_dense_rew_x_t1000/${i}/";
+  mkdir -p $EXP_PATH
+  python main.py ${DEFAULT_ARGS} --seed ${i} --log-dir ${EXP_PATH} --env-name InvertedPendulumSwingupVisionBulletEnv-v0 &
+
+  EXP_PATH="${LOG_DIR}/vime_baseline/inverted_pendulum_swingup_vision_dense_rew_x_t1000/${i}/";
+  mkdir -p $EXP_PATH
+  python main.py ${DEFAULT_ARGS} --vime --seed ${i} --log-dir ${EXP_PATH} --env-name InvertedPendulumSwingupVisionBulletEnv-v0 &
+
+  wait;
+done
