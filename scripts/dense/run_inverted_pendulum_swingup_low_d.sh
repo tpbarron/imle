9ff5670c33dc3fe6bf8fc7dca27a3eeeadd908f1
Trevor Barron <barron.trevor@gmail.com>
2017-10-30 18:31:29 -0700

    updating play to return avg

--- scripts/dense/run_inverted_pendulum_swingup_low_d.sh
+++ scripts/dense/run_inverted_pendulum_swingup_low_d.sh
@@ -1,5 +1,5 @@
 # LOG_DIR="/media/trevor/22c63957-b0cc-45b6-9d8f-173d9619fb73/outputs/imle/10_17_17/";
-# LOG_DIR="/media/Backup/trevor1_data/data/outputs/imle/10_20_17/set2/";
+LOG_DIR="/media/Backup/trevor1_data/data/outputs/imle/10_26_17/";
 # LOG_DIR="test_dir/";
 
 DEFAULT_ARGS="--bnn-n-updates-per-step 500 --max-episode-steps 1000 --num-processes 1 --num-steps 2048 --entropy-coef 0 --ppo-epoch 10 --lr 3e-4 --gamma 0.99 --tau 0.95 --batch-size 64 --num-frames 500000 --use-gae"
